7fd504a71d18d8b22d59568295482b548075ec29
Victor Pazaran <victor_hals@hotmail.com>
2023-03-07 08:59:42 -0600

    turning our spider's parse function into a generator to be able to save the results of our search as a file

--- quotes_scrapper/quotes_scrapper/spiders/quotes.py
+++ quotes_scrapper/quotes_scrapper/spiders/quotes.py
@@ -7,19 +7,15 @@ class QuotesSpider(scrapy.Spider):
    ]
 
    def parse(self, response):
-      print('*' * 10, '\n')
-      print(response.status, response.headers)
-      print('\n', '*' * 10)
-
       title = response.xpath('//h1/a/text()').get()
-      print(f'Titulo: {title}')
-
       quotes = response.xpath('//span[@class="text"]/text()').getall()
-      for quote in quotes:
-         print(f'- {quote}\n')
-
       top_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
-      for tag in top_tags:
-         print(f'- {tag}\n')
+
+      # In order to save the results as a file we should run "scrapy crawl quotes -o FILE.json or FILE.csv"
+      yield {
+         'title': title,
+         'quotes': quotes,
+         'top_ten_tags': top_tags
+      }
 
 
