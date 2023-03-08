0b514c8f7d7c2257773a1663da654f3ce3af500a
Victor Pazaran <victor_hals@hotmail.com>
2023-03-07 18:47:38 -0600

    modifiying our spider in order to navigate through all the next page buttons and get all the quotes

--- quotes_scrapper/quotes_scrapper/spiders/quotes.py
+++ quotes_scrapper/quotes_scrapper/spiders/quotes.py
@@ -5,17 +5,42 @@ class QuotesSpider(scrapy.Spider):
    start_urls = [
       'http://quotes.toscrape.com/'
    ]
+   custom_settings = {
+      'FEED_URI': 'quotes.json',
+      'FEED_FORMAT': 'json',
+      'overwrite': True
+   }
+
+   def parse_only_quotes(self, response, **kwargs):
+      if kwargs:
+         quotes = kwargs['quotes']
+      new_quotes = response.xpath('//span[@class="text"]/text()').getall()
+      quotes.extend(new_quotes)
+
+      next_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+      if next_button_link:
+         yield response.follow(next_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+      else:
+         yield {
+            'quotes': quotes
+         }
+
+
 
    def parse(self, response):
       title = response.xpath('//h1/a/text()').get()
       quotes = response.xpath('//span[@class="text"]/text()').getall()
       top_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
 
+
       # In order to save the results as a file we should run "scrapy crawl quotes -o FILE.json or FILE.csv"
       yield {
+         'url': response.url,
          'title': title,
-         'quotes': quotes,
          'top_ten_tags': top_tags
       }
 
+      next_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+      if next_button_link:
+         yield response.follow(next_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
 
